9b6a88aaf36ed95b85756a9ccf5f84aa6f093168
Fabrizhio Al chariti <fabrizhioal.2011@gmail.com>
2022-01-25 13:51:04 -0400

    comentarios en message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,15 @@
 const Message = (data, userData) => {
+    //verifica que los datos no esten vacios
     if (data === null) {
         alert('No se puede pasar de aca');
     }
+    //acomoda los mensajes en orden por fecha de envio
     data = data.sort((a, b) => {
         a = new Date(a.fecha);
         b = new Date(b.fecha);
         return a < b ? -1 : a > b ? 1 : 0;
     });
-
+    // verifica si el mensaje es del usuario o de otro
     let listMessages = data.map((x) => {
         let type =
             x.userName === userData.userName
